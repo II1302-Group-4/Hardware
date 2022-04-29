ed9af8a1c3aa8468494744d90636a0af448f04ce
jakobgotberg <jakob.gotberg@gmail.com>
2022-04-29 13:36:51 +0200

    Added a Main

---
+++ Main/Main.ino
@@ -0,0 +1,36 @@
+#include "CCS811.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial wifiSerial(2, 3);      // RX, TX for ESP8266
+//CCS811 ccs(A2, A3);
+
+bool DEBUG = true;   //show more logs
+int responseTime = 5000; //communication timeout
+
+void setup() {
+  Serial.begin(115200);
+  wifiSerial.begin(115200);
+  
+  //ccs.init();
+  //ccs.setReadInterval(ccs.INTERVAL_1SEC);
+
+  Serial.println("-----------------------------");
+  delay(200);
+  sendToWifi("AT+CWMODE=1",responseTime,DEBUG);
+  delay(1000);
+  sendToWifi("AT+CWJAP=\"Android Jakob\",\"leonboi11\"",responseTime,DEBUG);
+  delay(1000);
+  sendToWifi("AT+CIPSTART=\"TCP\",\"java.lab.ssvl.kth.se\",7",responseTime,DEBUG);
+  delay(1000);
+  sendData("Hej");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //ccs.fetchData();
+
+  //Serial.print(ccs.getCO2());
+
+
+}
