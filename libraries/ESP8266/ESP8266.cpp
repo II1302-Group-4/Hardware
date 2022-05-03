a5903e03a7dcd49d6c654b42c30167329a00fc41
Jakob Olsson <jakobolsson973@gmail.com>
2022-05-03 13:14:47 +0200

    Changing baudrate with UART cmd version

--- libraries/ESP8266/ESP8266.cpp
+++ libraries/ESP8266/ESP8266.cpp
@@ -1,16 +1,31 @@
 #include "ESP8266.h"
 
-ESP8266::ESP8266(int rx, int tx, int baud) {
+ESP8266::ESP8266(int rx, int tx, bool debug) {
     espSerial = new SoftwareSerial(rx, tx);
-    setBaud(baud);
+    DEBUG = debug;
+}
+
+void ESP8266::init() {
+    // Change baudrate of esp.
+    espSerial->begin(115200); // Default baud of esp is 115200.
+    espSerial->print("AT+UART_CUR=9600,8,1,0,0\r\n");
+
+    // Initialize software uart on same baud.
+    espSerial->begin(9600);
+    flushSerial();
+}
+
+void ESP8266::flushSerial() {
+    espSerial->flush();
+    while (espSerial->available())
+        espSerial->read();
 }
 
 void ESP8266::test() {
     sendCmd("AT");
 }
 
-void ESP8266::init() {
-    sendCmd("AT+RST");
+void ESP8266::clientMode() {
     sendCmd("AT+CWMODE=1");
 }
 
@@ -19,7 +34,7 @@ void ESP8266::connect(String ssid, String pwd) {
 }
 
 void ESP8266::openTCP(String ip, String port) {
-    sendCmd("AT+CIPSTART=\"TCP\",\"" + ip + "\",\"" + port + "\"");
+    sendCmd("AT+CIPSTART=\"TCP\",\"" + ip + "\"," + port);
 }
 
 void ESP8266::sendData(String data) {
@@ -30,41 +45,20 @@ void ESP8266::sendData(String data) {
 
 void ESP8266::sendCmd(String cmd) {
     espSerial->print(cmd + "\r\n");
+    if (DEBUG) {
+        Serial.print("sent: " + cmd + "\r\n");
+        Serial.print("resp: " + readResponse(1000));
+    }
 }
 
-String ESP8266::readResponse() {
-    readResponse(1000);
-    printIncoming();
-}
-
-String ESP8266::readResponse(int timeout) {
-    String response;
+String ESP8266::readResponse(const int timeout) {
+    String response = "";
     long int time = millis();
     while ((time+timeout) > millis()) {
-        while (espSerial->peek() != 13)
-            response = "" + espSerial->read();
+        while (espSerial->available()) {
+            char c = espSerial->read(); // read the next character.
+            response += c;
+        }
     }
     return response;
-}
-
-void ESP8266::printIncoming() {
-    delay(1000);
-    String msg = "";
-    Serial.println("--- Incoming data ---");
-    while (espSerial->available()) {
-        char c = espSerial->read();
-        Serial.println(c);
-        msg += c;
-    }
-    Serial.println(msg);
-    Serial.println("--- end of data ---");
-}
-
-void ESP8266::setBaud(int baud) {
-    int bauds [13] = {300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 31250, 38400, 57600, 115200};
-    for (int b : bauds) {
-        espSerial->begin(b);
-        espSerial->print("AT+IPR=" + baud + "\r\n")
-    }
-    espSerial->begin(baud);
 }
--- libraries/ESP8266/ESP8266.h
+++ libraries/ESP8266/ESP8266.h
@@ -6,18 +6,39 @@
 class ESP8266
 {
 public:
-    ESP8266(int rx, int tx, int baud);
-    void test();
+    ESP8266(int rx, int tx, bool debug);
     void init();
+    String test();
+    void clientMode();
     void connect(String ssid, String pwd);
     void openTCP(String ip, String port);
     void sendData(String data);
-    void sendCmd(String cmd);
-    String readResponse();
-    String readResponse(int timeout);
-    void printIncoming();
 private:
-    SoftwareSerial * espSerial = NULL;
-    void setBaud(int baud);
+    SoftwareSerial* espSerial = NULL;
+    void flushSerial();
+    String sendCmd(String cmd);
+    String readResponse(const int timeout);
+};
+
+#pragma once 
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+class ESP8266
+{
+public:
+    ESP8266(int rx, int tx, bool debug);
+    void init();
+    String test();
+    void clientMode();
+    void connect(String ssid, String pwd);
+    void openTCP(String ip, String port);
+    void sendData(String data);
+private:
+    SoftwareSerial* espSerial = NULL;
+    void flushSerial();
+    String sendCmd(String cmd);
+    String readResponse(const int timeout);
 };
 
