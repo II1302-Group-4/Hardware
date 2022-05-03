e55949a554f6e87da721c0931de4a64f2d17bc01
jakobgotberg <jakob.gotberg@gmail.com>
2022-05-03 16:03:34 +0200

    Testing http

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,13 +2,20 @@
 
 ESP8266 esp(2, 3, true);
 
+String url = "ptsv2.com/t/e1oit-1651498913";
+String test_ip = "216.239.34.21";
+String daytime = "java.lab.ssvl.kth.se";
+
 void setup() {
     Serial.begin(9600);
     Serial.println("\n------------------------");
     esp.init();
-    esp.test();
     esp.clientMode();
     esp.connect("Android Jakob", "leonboi11");
+    esp.openTCP(test_ip,"80");
+    //esp.httpPostTest("this is a text about birds.");
+    esp.sendCmd("this is a text");
+    esp.closeAll();
 }
 
 void loop() {
