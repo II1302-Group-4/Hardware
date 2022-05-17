61f426073c7041bed080f68bcb18aa5bf2e12218
Jakob Olsson <jakobolsson973@gmail.com>
2022-05-17 22:10:09 +0200

    Use delay addition and querying timeserver to keep track of time

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,6 +4,7 @@ const String SERVER = "pollusenseserver.azurewebsites.net";
 const String SERVER_PORT = "80";
 const String DAYTIME_SERVER = "java.lab.ssvl.kth.se";
 const String DAYTIME_SERVER_PORT = "13";
+int loopLap = 3;
 long unixTime = 0;
 const int GREEN_LED = 8;
 const int RED_LED = 9;
@@ -25,34 +26,37 @@ void setup() {
     
     // Initialize wifi communication
     pollu.wifiModule->init();
-    unixTime = 0;
     while (true) {
-        if (!pollu.wifiModule->connectToAP(pollu.wifiModule->ssid, pollu.wifiModule->pwd))
-            continue;
-        else
+        if (pollu.wifiModule->connectToAP(pollu.wifiModule->ssid, pollu.wifiModule->pwd))
             break;
     }
-    greenHighRedLow();
-    
+    greenHighRedLow(); 
 }
 
 void loop() {
     // Get sensor values and increment timer
-    while(unixTime == 0)
-    {
-        pollu.wifiModule->closeTCP();
-        delay(1000);
-        unixTime = pollu.getEpoch(DAYTIME_SERVER, DAYTIME_SERVER_PORT);
-        delay(200);
-        Serial.println("=== unixtime ===");
-        Serial.println(unixTime);
+    unixTime += loopDelay;
+    Serial.print("Lap:");
+    Serial.println(loopLap);
+    if (loopLap++ == 3) {
+        unixTime = 0;
+        while(unixTime == 0)
+        {
+            pollu.wifiModule->closeTCP();
+            delay(1000);
+            unixTime = pollu.getEpoch(DAYTIME_SERVER, DAYTIME_SERVER_PORT);
+            delay(200);
+            Serial.println("=== unixtime ===");
+            Serial.println(unixTime);
+        }
+        loopLap = 0;
     }
+
     pollu.sensorModule->fetchData();
     String voc = String(pollu.sensorModule->getVOC());
     String co2 = String(pollu.sensorModule->getCO2());
-    unixTime += loopDelay;
 
-// Put switch-case into loop?
+    // TODO: Put switch-case into loop?
     // Make a post to the database
     switch (pollu.wifiModule->status()) {
         case 1:
@@ -69,8 +73,7 @@ void loop() {
             pollu.postData(String(unixTime), voc, co2);
     }
 
-    unixTime = 0;
-    wait(45);
+    wait(loopDelay);
 }
 
 void greenHighRedLow() {
